7684ba6b007ab9671328eb929fade6b6f9bbbb30
emilyorsborne <emorsborne@gmail.com>
2018-02-08 17:56:44 -0500

    draft3

--- react_spa/src/Contact.js
+++ react_spa/src/Contact.js
@@ -5,27 +5,9 @@ class Contact extends Component {
   render() {
     return (
       <div>
-        <h2>GOT QUESTIONS?</h2>
-        <p>The easiest thing to do is post on
-        our <a href="http://forum.kirupa.com">forums</a>.
+        <h2>Search Locations</h2>
+        <p>GoogleMaps <a href="http://googlemaps.com">Search</a>.
         </p>
-        <table class="countdownContainer">
-            <tr class= "info">
-            <td colspan="4">Countdown until the New Year!</td>
-            </tr>
-            <tr class="info">
-                <td id = "days">328</td>
-                <td id = "hours">7</td>
-                <td id = "minutes">20</td>
-                <td id = "seconds">4</td>
-            </tr>
-            <tr>
-                <td>Days</td>
-                <td>Hours</td>
-                <td>Minutes</td>
-                <td>Seconds</td>
-            </tr>
-        </table>
       </div>
     );
   }
